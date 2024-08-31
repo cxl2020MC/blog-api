40d9052c82c9530156a27da5e2f164efa0833885
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-31 11:13:00 +0800

    Update db.py

--- core/db.py
+++ core/db.py
@@ -3,7 +3,7 @@ import os
 
 # 狗屎vercel的事件循环好像是一次性的 这样写不行（
 '''
-db_url = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
+db_url = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
 
 _client = motor_asyncio.AsyncIOMotorClient(db_url)
 db = _client["blog_nuxt"]
@@ -13,7 +13,7 @@ posts = db["posts"]
 '''
 
 def get_db():
-  db_url = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
+  db_url = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
   _client = motor_asyncio.AsyncIOMotorClient(db_url)
   db = _client["blog_nuxt"]
   return db
