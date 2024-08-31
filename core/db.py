b7a17e2e867ab4d7ca3b0a9cce98daf887964c49
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-31 11:50:45 +0800

    Update db.py

--- core/db.py
+++ core/db.py
@@ -1,7 +1,7 @@
 from motor import motor_asyncio
 import os
 
-# 狗屎vercel的事件循环好像是一次性的 这样写不行（
+# 狗屎vercel的事件循环好像一个请求新建一个新的 这样写不行（
 '''
 db_url = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
 
@@ -11,9 +11,27 @@ db = _client["blog_nuxt"]
 users = db["users"]
 posts = db["posts"]
 '''
-
+# 这个理论上会炸内存
+'''
 def get_db():
   db_url = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
   _client = motor_asyncio.AsyncIOMotorClient(db_url)
   db = _client["blog_nuxt"]
   return db
+'''
+
+# 拿class写个用with自动销毁连接的
+class _DB:
+  def __init__(self, db_uri: str = os.getenv("MONGODB_URI", "mongodb://localhost:27017")):
+    self.db_uri = db_uri
+  async def __aenter__(self):
+    self._client = motor_asyncio.AsyncIOMotorClient(db_uri)
+    self.db = _client["blog_nuxt"]
+    print("创建数据库连接")
+
+  async def __aexit__(self):
+    await self._client.close()
+    print("数据库连接已关闭")
+  
+  
+  
