e90ad05f1408bc45160ddf33179e4fefd251fd31
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-09-28 10:28:40 +0800

    Update main.py

--- core/main.py
+++ core/main.py
@@ -5,28 +5,35 @@ from . import models
 
 router = APIRouter()
 
-
-
 @router.get("/")
 async def root():
     return utils.return_data(msg = "博客api正常运行")
 
-@router.get("/post/{link}")
-async def 获取文章内容(link:str):
-    async with DB() as db:
-        data = await db.posts.find_one({"link": link, "_id": 0})
-        return utils.return_data(data)
-
-@router.post("/post")
-async def 上传文章(data: models.Post):
-    async with DB() as db:
-        await db.posts.insert_one(dict(data))
-        return utils.return_data(msg = "上传成功")
-
-@router.get("/postlist")
+@router.get("/posts")
 async def 获取文章列表():
     async with DB() as db:
        data = await db.posts.find({}, {"md_content": 0}).to_list(length=100)
        data = utils.id转换(data)
        print(data)
        return utils.return_data(data)
+
+@router.get("/posts")
+async def 获取文章列表():
+        async with  {"md_content"      data = await db.posts.find({}, {"md_content": 0})a)
+           ngth=100)
+       data = utils.id转换(data)
+       print(data)
+
+@router.get("/posts/{link}")
+async def 获取文章内容(link:str):
+    async with DB() as db:
+        data = await db.posts.find_one({"link": link, "_id": 0})
+        return utils.return_data(data)
+
+@router.post("/posts")
+async def 上传文章(data: models.Post):
+    async with DB() as db:
+        await db.posts.insert_one(dict(data))
+        return utils.return_data(msg = "上传成功")
+
+
