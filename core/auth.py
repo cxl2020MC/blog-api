d152520805ef85924a44268742982b55bdcf53cf
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2025-02-05 07:27:11 +0000

    test

---
+++ core/auth.py
@@ -0,0 +1,12 @@
+import datetime
+import jwt
+import os
+
+jwt_secret = os.getenv("JWT_SECRET", "1145141919810")
+
+jwt_change = datetime.timedelta(days=30)
+
+
+def create_access_token(username: str, password: str):
+    pass
+
